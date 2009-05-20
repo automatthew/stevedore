c26eadc13f03efec72dc534966297a6551aa0eb6
Matthew King <automatthew@gmail.com>
2009-05-20 11:28:53 -0500

    fixed stupid median bug

--- lib/stevedore/stats.rb
+++ lib/stevedore/stats.rb
@@ -1,9 +1,14 @@
 class Stevedore
   
   module Stats
+    # require 'ffi'
+    # extend FFI::Library
+    # attach_function :sqrt, [:double], :double
+    # 
+    # 
     
     def sum
-      s = 0; self.each { |element| s = s + element }; s
+      s = 0; self.each { |element| s += element }; s
     end
     
     def mean
@@ -11,11 +16,10 @@ class Stevedore
     end
     
     def variance
-      n = self.size
-      denom = n - 1
+      denom = self.size - 1
       m = self.mean
       s = 0
-      self.each { |element| s = s + (element - m)**2; }
+      self.each { |element| s += (element - m)**2; }
       s / denom
     end
     
@@ -24,16 +28,18 @@ class Stevedore
     end
     
     def median
-      sorted = self.sort; i = sorted.size % 2
+      sorted = self.sort; 
+      s = sorted.size
+      i = s % 2
       case i
-       when 0 then sorted[i/2 - 1, 2].mean
-       when 1 then sorted[i/2].to_f
+       when 0 then sorted[s/2 - 1, 2].mean
+       when 1 then sorted[s/2].to_f
       end if sorted.size > 0
     end
     
-    def self.power_test(*args)
-      Stevedore::ShellR.new.power_test(*args)
-    end
+    # def self.power_test(*args)
+    #   Stevedore::ShellR.new.power_test(*args)
+    # end
         
   end
   
--- test/test_sample.rb
+++ test/test_sample.rb
@@ -6,7 +6,7 @@ describe "Stevedore::Sample" do
   
   before do
     @sample = Sample.new((1..40).to_a)
-    @nested_sample = Sample.new [Sample.new((1..40).to_a), Sample.new((1..40).to_a), Sample.new((1..40).to_a) ]
+    @nested_sample = Sample.new [Sample.new((1..40).to_a), Sample.new((41..80).to_a), Sample.new((81..119).to_a) ]
   end
   
   it "a flattened Sample is still a Sample" do
@@ -16,12 +16,17 @@ describe "Stevedore::Sample" do
   
   it "has a mean" do
     @sample.mean.should == 20.5
-    @nested_sample.mean.should == 20.5
+    @nested_sample.mean.should == 60
+  end
+  
+  it "has a median" do
+    @sample.median.should == 20.5
+    @nested_sample.median.should == 60
   end
   
   it "has a standard deviation" do
     @sample.standard_deviation.to_i.should == 11
-    @nested_sample.standard_deviation.to_i.should == 11
+    @nested_sample.standard_deviation.to_i.should == 34
   end
   
   it "can become simple arrays" do
